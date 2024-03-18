a92aa82ac6aa6556d5c8ae747fc69abdcf44dc80
llazzari <lucas.lazzari@outlook.com>
2024-03-18 14:58:31 -0300

    fix: add update load options when saving new patient

--- src/components/save_data_button.py
+++ src/components/save_data_button.py
@@ -1,15 +1,17 @@
-from dash import Dash, Input, Output, State
+from dash import Dash, Input, Output, State, no_update
 import dash_mantine_components as dmc
+from typing import Any
 
-from data import cars
 from components import ids
+from data import cars
+from data.patient import get_patient_names
 
 
 def render(app: Dash) -> dmc.Button:
     @app.callback(
         Output(ids.ALERT_SAVE, 'hide'),
         Output(ids.ALERT_SAVE, 'children'),
-        # Output(ids.ALERT_SAVE, 'title'),
+        Output(ids.SELECT_PATIENT, 'data'),
         Input(ids.SAVE_BUTTON, 'n_clicks'),
         [
             State(ids.NAME, 'value'),
@@ -27,14 +29,14 @@ def render(app: Dash) -> dmc.Button:
         date: str,
         scores: list[float],
         observations: list[str]
-    ) -> tuple[bool, str]:  # , str]:
+    ) -> tuple[bool, str, Any]:
         if not name:
-            return False, 'Preencha o nome.'  # , 'Erro'
+            return False, 'Preencha o nome.', no_update
 
         patient_data: dict[str, str] = {'name': name, 'birthdate': birthdate}
         cars.insert_scores(patient_data, date, scores, observations)
 
-        return False, 'Os dados foram salvos com sucesso!'  # , 'Sucesso'
+        return False, 'Os dados foram salvos com sucesso!', get_patient_names()
     return dmc.Button(
         'Salvar',
         id=ids.SAVE_BUTTON,
